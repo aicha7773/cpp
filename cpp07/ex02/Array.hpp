2cf318ce9b53a3c3972cf152b44a0dfbad7ff60b
Aicha Atki <aatki@c4r9p7.1337.ma>
2024-01-09 18:44:00 +0100

    push

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: aatki <aatki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 05:43:48 by aatki             #+#    #+#             */
-/*   Updated: 2024/01/08 19:28:51 by aatki            ###   ########.fr       */
+/*   Updated: 2024/01/09 18:43:11 by aatki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,22 +23,22 @@ class Array
     public:
         Array(){ tab = new T; asize =0;}
         Array(unsigned int n){tab = new T[n] ;asize = n;}
-        ~Array(){delete tab;}
-        Array(Array &other) 
+        ~Array(){delete[] tab;}
+        Array(Array &other)
         {
             asize = other.asize;
-
             tab = new T[asize];
             for (unsigned int i = 0;i<asize;i++)
                 tab[i] = other.tab[i];
         }
-        Array &operator=(Array &other) const
+        Array &operator=(Array &other)
         {
-            delete tab;
-            asize = other.size;
+            delete[] tab;
+            asize = other.size();
             tab = new T[asize];
-            for (unsigned int i;i<asize;i++)
+            for (unsigned int i=0;i<asize;i++)
                 tab[i] = other.tab[i];
+            return *this;
         }
         unsigned int size() const
         {
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,81 +6,81 @@
 /*   By: aatki <aatki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 05:43:45 by aatki             #+#    #+#             */
-/*   Updated: 2024/01/08 19:29:28 by aatki            ###   ########.fr       */
+/*   Updated: 2024/01/09 18:39:20 by aatki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-// int main()
-// {
-//     const Array<int> ar(3);
-    
-//     // ar2 = ar;
-//     // try{
-
-//     //     ar[0]=1;
-//     //     ar[1]=9000000;
-//     //     int o=ar[2]=3;
-//     //     std::cout<<o<<std::endl;
-//     // for (int i=0;i<3;i++)
-//     //     std::cout<<ar[i]<<std::endl;
-//     // }
-//     // catch(std::exception& e){
-//     //     std::cout << e.what() << std::endl;
-//     // }
-// }
-
-
-#include <iostream>
 #include "Array.hpp"
 
-#define MAX_VAL 750
-int main(int, char**)
+int main()
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+    Array<int> ar(3);
+    Array<int> ar2(7);
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
+    ar2 = ar;
+    try{
+        ar[0]=1;
+        ar[1]=9000000;
+        int o=ar[2]=3;
+        std::cout<<o<<std::endl;
+    for (int i=0;i<3;i++)
+        std::cout<<ar[i]<<std::endl;
     }
-    try
-    {
-        numbers[-2] = 0;
+    catch(std::exception& e){
+        std::cout << e.what() << std::endl;
     }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
-    return 0;
 }
+
+
+// #include <iostream>
+
+// #define MAX_VAL 750
+// int main(int, char**)
+// {
+//     Array<int> numbers(MAX_VAL);
+//     int* mirror = new int[MAX_VAL];
+//     srand(time(NULL));
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         const int value = rand();
+//         numbers[i] = value;
+//         mirror[i] = value;
+//     }
+//     //SCOPE
+//     {
+//         Array<int> tmp = numbers;
+//         Array<int> test(tmp);
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         if (mirror[i] != numbers[i])
+//         {
+//             std::cerr << "didn't save the same value!!" << std::endl;
+//             return 1;
+//         }
+//     }
+//     try
+//     {
+//         numbers[-2] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+//     try
+//     {
+//         numbers[MAX_VAL] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         numbers[i] = rand();
+//     }
+//     delete [] mirror;//
+//     return 0;
+// }
