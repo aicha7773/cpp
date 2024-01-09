441e344df7a8a7a27cce9a41ff75364c4656fccf
Aicha Atki <aatki@c4r9p7.1337.ma>
2024-01-09 18:44:43 +0100

    push

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aatki <aatki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 04:17:15 by aatki             #+#    #+#             */
-/*   Updated: 2024/01/08 22:40:14 by aatki            ###   ########.fr       */
+/*   Updated: 2024/01/09 03:37:58 by aatki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,3 +28,33 @@ int main()
     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
     return 0;
 }
+
+// class Awesome
+// {
+//   public:
+//     Awesome(void) : _n(0) {}
+//     Awesome( int n ) : _n( n ) {}
+//     Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+//     bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+//     bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+//     bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+//     bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+//     bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+//     bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+//     int get_n() const { return _n; }
+//   private:
+//     int _n;
+// };
+
+// std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
+
+// int main(void)
+// {
+// 	Awesome a(2), b(4);
+
+//         swap(a, b);
+//         std::cout << a << " " << b << std::endl;
+//         std::cout << max(a, b) << std::endl;
+//         std::cout << min(a, b) << std::endl;
+//         return (0);
+// }
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,47 +6,15 @@
 /*   By: aatki <aatki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 05:07:29 by aatki             #+#    #+#             */
-/*   Updated: 2024/01/09 01:28:12 by aatki            ###   ########.fr       */
+/*   Updated: 2024/01/09 18:25:26 by aatki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-// int main()
-// {
-//     int tab[]={1,2,3};
-
-//     iter(tab, 3, &function< int>);
-// }
-
-class Awesome
+int main()
 {
-  public:
-    Awesome( void ) : _n( 42 ) { return; }
-    int get( void ) const { return this->_n; }
-  private:
-    int _n;
-};
-
-std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
-{
-  o << rhs.get();
-  return o;
+    int tab[]={1,2,3};
+    iter(tab, 3, &function<unsigned int>);
 }
 
-template< typename T >
-
-void print( T& x )
-{
-  std::cout << x << std::endl;
-  return;
-}
-
-int main() {
-int tab[] = { 0, 1, 2, 3, 4 };
-Awesome tab2[5];
-
-  iter( tab, 5, print<const int> );
-  iter( tab2, 5, print<Awesome> );
-  return 0;
-}
