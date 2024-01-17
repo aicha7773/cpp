d0ffed4483c2f4d90a84a947c34605b5cff352ca
Aicha Atki <aatki@e2r8p7.1337.ma>
2024-01-17 23:50:22 +0100

    push

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -6,19 +6,50 @@
 /*   By: aatki <aatki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:37:38 by aatki             #+#    #+#             */
-/*   Updated: 2024/01/17 17:08:04 by aatki            ###   ########.fr       */
+/*   Updated: 2024/01/17 23:49:00 by aatki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int CheckRanges(long long tab)
+std::map<std::string,int>    ParsingData()
+{
+    std::map <std::string,int> mapy;
+    std::string line;
+    std::ifstream data("data.csv");
+    if (data.is_open())
+    {
+        getline(data,line);
+        while (getline(data,line))
+            mapy[line.substr(0,10)] = stoi(line.substr(11,line.size() - 11));
+    }
+    else
+        std::cout<<"Unable to open file\n";
+    return mapy;
+}
+
+int isKabissa(int annee)
+{
+    if ((annee - 2008) % 4 == 0)
+        return 1;
+    return 0;
+}
+
+int CheckRanges(long long *tab)
 {
     if (tab [0] < 2009 || tab[0] > 2022)
         return 0;
-        if (tab [0] < 2009 || tab[0] > 2022)
+    if (tab [1] < 1|| tab[1] > 12)
         return 0;
-        if (tab [0] < 2009 || tab[0] > 2022)
+    if (tab [2] < 1 || tab[2] > 31)
+        return 0;
+    if (tab [3] < 0 || tab[3] > 1000)
+        return 0;
+    if (tab[1] == 2 && tab[2] > 29)
+        return 0;
+    if (!isKabissa(tab[0]) && tab[1] == 2 && tab[2] == 29)
+        return 0;
+    if (tab[2] > 30 && (tab[1] != 1 && tab[1] != 3 && tab[1] != 5 &&tab[1] != 7 &&tab[1] != 8 && tab[1] != 10 && tab[1] != 12) )
         return 0;
     return 1;
 }
@@ -59,23 +90,27 @@ int CheckLine(std::string line)
 
 void ParsingInputFile(std::string input)
 {
+    std::map<<std::string, int> mapy = ParsingData();
     std::string line;
+    int value;
     std::ifstream myfile (input.c_str());
     if (myfile.is_open())
     {
         getline (myfile,line);
         if (line != "date | value")
         {
-            std::cout <<"an error has been found\n";
+            std::cout <<"an error has been occurend\n";
             return ;
         }
         while (getline (myfile,line))
         {
+            value =  mapy[line.substr(0,10)] * stoi(line.substr(13,line.size() - 12));
             if (!CheckLine(line))
             {
-                std::cout <<"an error has been found\n";
+                std::cout <<"an error has been occurend\n";
                 return ;
             }
+            std::cout<<line.substr(0,10)<<" = "<<value<<std::endl;
         }
         myfile.close();
         std::cout <<"everyting workin clean\n";
--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aatki <aatki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:37:35 by aatki             #+#    #+#             */
-/*   Updated: 2024/01/17 15:53:10 by aatki            ###   ########.fr       */
+/*   Updated: 2024/01/17 22:43:19 by aatki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int main(int ac, char **av)
     
     if( ac == 2)
     {
-        ParsingInputFile(av[1]);
         
+        ParsingInputFile(av[1]);
     }
     return 0;
 }
